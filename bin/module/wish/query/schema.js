de9905fc57cf450a9e33f3e3c95548a742af792b
Imam Syuhada <imamsy11@gmail.com>
2022-01-24 22:44:40 +0700

    default size of list wishes (5)

--- bin/module/wish/query/schema.js
+++ bin/module/wish/query/schema.js
@@ -3,7 +3,7 @@ const joi = require('joi');
 const listWishes =  joi.object({
   sort: joi.object().unknown().optional().default({ _id: -1 }),
   page: joi.number().optional().default(1),
-  size: joi.number().optional().default(10),
+  size: joi.number().optional().default(5),
 });
 
 module.exports = {
