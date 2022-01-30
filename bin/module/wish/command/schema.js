f069db662a9546beefbb9ee15b7db0eb37043c86
Imam Syuhada <imamsy11@gmail.com>
2022-01-31 02:21:18 +0700

    change schema postWish

--- bin/module/wish/command/schema.js
+++ bin/module/wish/command/schema.js
@@ -2,9 +2,9 @@ const joi = require('joi');
 const moment = require('moment-timezone');
 
 const postWish =  joi.object({
-  author: joi.string().min(1).max(25).required(),
+  author: joi.string().min(1).max(64).required(),
   attendace: joi.string().valid('hadir', 'tidak hadir', 'mungkin hadir').required(),
-  wish: joi.string().min(1).max(128).required(),
+  wish: joi.string().min(1).max(256).required(),
   updatedAt: joi.date().forbidden().default(moment.tz(Date.now(), 'Asia/Jakarta')),
   createdAt: joi.date().forbidden().default(joi.ref('updatedAt')),
 });
