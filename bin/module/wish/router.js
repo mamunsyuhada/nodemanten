9cad61f0e42f9350cc68de0b1c0bbf5154c845c4
Imam Syuhada <imamsy11@gmail.com>
2022-01-31 01:46:17 +0700

    change response

--- bin/module/wish/router.js
+++ bin/module/wish/router.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const apiHandler = require('./apiHandler');
 const apiLimiter = require('../../middleware/rateLimiter');
 
-router.post('/', apiLimiter(5, 1, 'limited, wait for 5 minutes'), apiHandler.postWish);
+router.post('/', apiLimiter(10, 1, 'tunggu sebentar (sekitar 10 menit) untuk mengirimkan pesan'), apiHandler.postWish);
 router.get('/', apiHandler.listWishes);
 
 module.exports = router;
