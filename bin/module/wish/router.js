4186998533bae17e5de9ae4888ee80a7d26c0f68
Mamun <imam.sy11@gmail.com>
2022-01-30 21:39:49 +0700

    Update router.js

--- bin/module/wish/router.js
+++ bin/module/wish/router.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const apiHandler = require('./apiHandler');
 const bruteForce = require('../../util/bruteLimiter');
 
-router.post('/', bruteForce.prevent, apiHandler.postWish);
+router.post('/', apiHandler.postWish);
 router.get('/', apiHandler.listWishes);
 
 module.exports = router;
