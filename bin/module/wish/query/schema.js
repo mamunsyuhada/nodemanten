cad3a3e73fd8dc82bc63583c3766394d50eb4805
Imam Syuhada <imamsy11@gmail.com>
2022-02-02 21:40:57 +0700

    change sort by

--- bin/module/wish/query/schema.js
+++ bin/module/wish/query/schema.js
@@ -1,7 +1,7 @@
 const joi = require('joi');
 
 const listWishes =  joi.object({
-  sort: joi.object().unknown().optional().default({ _id: -1 }),
+  sort: joi.object().unknown().optional().default({ createdAt: -1 }),
   page: joi.number().optional().default(1),
   size: joi.number().optional().default(5),
 });
