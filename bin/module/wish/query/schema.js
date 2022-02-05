df339d6b68c14f22dade0c8a9d78e6e6b5177858
Imam Syuhada <imamsy11@gmail.com>
2022-02-05 15:00:43 +0700

    add userAgent

--- bin/module/wish/query/schema.js
+++ bin/module/wish/query/schema.js
@@ -4,6 +4,7 @@ const listWishes =  joi.object({
   sort: joi.object().unknown().optional().default({ createdAt: -1 }),
   page: joi.number().optional().default(1),
   size: joi.number().optional().default(5),
+  userAgent: joi.string().optional(),
 });
 
 module.exports = {
